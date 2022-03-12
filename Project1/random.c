47f9409a54528dbd69f52153424382dc6273d6ba
scnelMG <qkralsrb4407@naver.com>
2022-03-13 04:21:07 +0900

    random

--- Project1/random.c
+++ Project1/random.c
@@ -4,7 +4,7 @@
 int main_random(void)
 {
 	/*srand(time(NULL));  //난수 초기화
-	int num = rand() % 3 + 1; // 1 ~ 3 중 뽑는다*/
+	int num = rand() % 3 + 1; // 1 ~ 3 중 랜덤*/
 
 	printf("난수 초기화 이전..\n");
 	for (int i = 0; i < 19; i++)
